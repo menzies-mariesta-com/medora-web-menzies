b27e875b91c0b91317b6cd698ea9ffee4ef1790f
AungKaungMyat9604 <aungkaungmyat9604@gmail.com>
2026-03-25 13:31:48 +0630

    feature--implement-patient-visit-history-dashboard-and-fix-order-bugs

--- src/routes/(private)/heka/hospital/[hospital_id]/home/observation/emr/patient-visit-history-dashboard/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import LPatientVisitHistoryDashboard from '$lib/component/local/private/heka/visit/LPatientVisitHistoryDashboard.svelte';
-</script>
-
-<LPatientVisitHistoryDashboard moduleKey="observation" />
